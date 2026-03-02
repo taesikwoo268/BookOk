b801bb78285a3d8688434e56a89faa1fa47cd27f
taesikwoo268 <24020171@vnu.edu.vn>
2026-03-02 22:06:06 +0700

    add get,post,delete

---
+++ src/test/java/com/example/bookok/BookOkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
